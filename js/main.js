bb571f19b2b3a9683d78b251a01fd197991c44f4
ayman-el-khateeb786 <126987333+ayman-el-khateeb786@users.noreply.github.com>
2024-08-01 22:54:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+function show_hide() {
+
+  var a = document.getElementById("links-animat");
+
+  if (a.style.display === "none") {
+    a.style.display = "flex";
+  }
+  else {
+    a.style.display = "none";
+  }
+}
+
+
+function show_hide1() {
+
+  var a = document.getElementById("lorem1");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+  }
+  else {
+    a.style.display = "none";
+  }
+}
+function show_hide2() {
+
+  var a = document.getElementById("lorem2");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+  }
+  else {
+    a.style.display = "none";
+  }
+}
+function show_hide3() {
+
+  var a = document.getElementById("lorem3");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+  }
+  else {
+    a.style.display = "none";
+  }
+}
+function show_hide4() {
+
+  var a = document.getElementById("lorem4");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+  }
+  else {
+    a.style.display = "none";
+  }
+}
